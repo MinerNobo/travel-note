941f8755a2b917b3f54d7fdd3dc2f06fed87e948
chengning <ningc673@gmail.com>
2025-05-05 04:49:39 +0800

    feat: 增加tabbar icon；修改部分组件样式

--- src/app.config.ts
+++ src/app.config.ts
@@ -7,28 +7,35 @@ export default defineAppConfig({
     navigationBarTextStyle: "black",
   },
   tabBar: {
-    custom: false, // 先用微信默认的tabbar
+    custom: false, // 用微信默认的tabbar
     color: "7A7E83",
-    selectedColor: "#3cc51f", // 文字选中颜色
+    selectedColor: "#1890ff", // 文字选中颜色
     backgroundColor: "#ffffff", // 背景色
     borderStyle: "black", // 边框颜色
-    // TODO：还需要选择tabBar的icon
     list: [
       {
         pagePath: "pages/index/index",
         text: "首页",
+        iconPath: "assets/tabbar/home.png",
+        selectedIconPath: "assets/tabbar/home-active.png"
       },
       {
         pagePath: "pages/myNote/index",
         text: "游记",
+        iconPath: "assets/tabbar/mynote.png",
+        selectedIconPath: "assets/tabbar/mynote-active.png"
       },
       {
         pagePath: "pages/publish/index",
         text: "发布",
+        iconPath: "assets/tabbar/publish.png",
+        selectedIconPath: "assets/tabbar/publish-active.png"
       },
       {
         pagePath: "pages/mine/index",
         text: "我的",
+        iconPath: "assets/tabbar/mine.png",
+        selectedIconPath: "assets/tabbar/mine-active.png"
       },
     ],
   },
